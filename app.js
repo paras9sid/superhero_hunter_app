a9a6f89315d22031c8507091a2c08edab6afc501
paras9sid <sidharthjainp9@gmail.com>
2023-03-02 20:47:23 +0530

    project setup done

---
+++ app.js
@@ -0,0 +1,22 @@
+// import MD5 from "crypto-js/md5";
+// console.log(MD5("text to hash").toString());
+
+let tasks = [];
+
+const tasksList = document.getElementById("list");
+
+const addTaskInput = document.getElementById("add");
+
+console.log("Working"); //check app is working or not in console
+
+function renderList() {}
+
+function toggleTask(taskId) {}
+
+function deleteTask(taskId) {}
+
+function addTask(task) {}
+
+function showNotification(text) {
+  alert(text);
+}
